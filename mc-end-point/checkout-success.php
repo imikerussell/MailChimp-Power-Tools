19622ff7fddecd64ce80f2c1ef4dc78d680ebf2f
Mike Russell <mike@musicradiocreative.com>
2017-01-21 18:35:41 +0000

    Added checkout-success.php for ecommerce stores that would like to subscribe customers

---
+++ mc-end-point/checkout-success.php
@@ -0,0 +1,29 @@
+<?php
+	// load in mailchimp library
+	include('../mailchimp-api/src/MailChimp.php');
+	include 'config.php';
+	
+	// namespace defined in MailChimp.php
+	use \DrewM\MailChimp\MailChimp;
+	
+	// connect to mailchimp
+	$MailChimp = new MailChimp($mcapi); // put your API key here
+	$list = $mclistid; // put your list ID here
+	$email = $_GET['EMAIL']; // Get email address from form
+	$id = md5(strtolower($email)); // Encrypt the email address
+	// setup the merge fields
+	$mergeFields = array(
+		// *** YOUR FIELDS GO HERE ***
+		 'FNAME'=>$_GET['FNAME'],
+ 		 'CHECKOUT' => 'True',
+		);
+	// remove empty merge fields
+	$mergeFields = array_filter($mergeFields);
+	$result = $MailChimp->put("lists/$list/members/$id", array(
+									'email_address'     => $email,
+									'status'            => 'subscribed',
+									'merge_fields'      => $mergeFields,
+									'update_existing'   => true, // YES, update old subscribers!
+							));
+	echo json_encode($result);
+	header( 'Location: ' . ($thanksurl) . '' ) ; ?>
